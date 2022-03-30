7def9c156a520f25f899feedd955954e960b3475
zhong mingyuan <mralenzhong@qq.com>
2022-03-30 22:01:38 +0800

    symbol的实现

---
+++ src/day3/allSettled.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: MrAlenZhong
+ * @Date: 2022-03-30 21:02:23
+ * @LastEditors: MrAlenZhong
+ * @LastEditTime: 2022-03-30 21:02:24
+ * @Description: Promise.allSettled的实现
+ */
+
+Promise.allSettled
